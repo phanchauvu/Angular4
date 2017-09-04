50db33761bfcd72340b19b71503e0f0ea3a2c2c9
Phan Chau Vu <phanchauvu2808@gmail.com>
2017-09-04 17:30:43 +0700

    Create First Component Angular

---
+++ src/app/Components/Khoahoc/khoahoc.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-khoahoc',
+    templateUrl: './khoahoc.component.html',
+    styleUrls: ['./khoahoc.component.css']
+})
+
+export class KhoahocComponent {
+    title = "Khoa hoc"
+}
